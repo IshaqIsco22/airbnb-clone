f5bb5f54f5e429390d9479d0701f1a3143c42744
IshaqIsco22 <ishanpkd786@gmail.com>
2024-01-09 11:02:07 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-target-blank */
+import "./App.css";
+import { FaAirbnb } from "react-icons/fa";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="" className="logo">
+          <FaAirbnb />
+          <span>airbnb</span>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
